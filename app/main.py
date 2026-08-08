2f1c9a62e404962bd83b168dc1565361c6f5f196
Pedro Pimpão <pedrohmpimpao@gmail.com>
2026-08-08 20:54:48 -0300

    chore(api): configurar CORS para o frontend local

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routes.auth_routes import auth_router
 from .routes.url_routes import url_router
@@ -9,6 +10,17 @@ app = FastAPI()
 app.include_router(auth_router)
 app.include_router(url_router)
 app.include_router(user_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    allow_headers=["Authorization", "Content-Type"],
+)
 
 @app.get("/")
 async def root():
